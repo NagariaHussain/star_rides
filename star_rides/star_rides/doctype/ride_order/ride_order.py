9bc4232352710cde93e59ea14f32746b3939dce1
Hussain Nagaria <hussainbhaitech@gmail.com>
2022-11-18 12:39:15 +0530

    feat: Vehicle booking webform

--- star_rides/star_rides/doctype/ride_order/ride_order.py
+++ star_rides/star_rides/doctype/ride_order/ride_order.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2022, Hussain and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class RideOrder(Document):
-	pass
+    def before_insert(self):
+        if not self.customer:
+            self.customer = frappe.session.user
